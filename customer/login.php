b82b7227bba2ad43917dec31df1d3fdde36509a5
binhnguyen3816 <binhnguyen3816@gmail.com>
2023-11-18 14:13:35 +0700

    update contact.php & policy.php

---
+++ customer/login.php
@@ -0,0 +1,152 @@
+<?php
+session_start();
+ob_start();
+$rootPath = '/Lap_trinh_web/';
+if (isset($_SESSION['email_user']) && !empty($_SESSION['email_user'])) {
+  if (isset($_SESSION['cart']) && !empty($_SESSION['cart']))
+    header('location: check_out.php');
+  else
+    header('location: my_account.php');
+}
+
+require_once '../database/DB.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet"  href="https://site-assets.fontawesome.com/releases/v6.1.2/css/all.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+<body>
+<?php 
+    require '../includes/header.php';
+    // require '../includes/navbar.php';
+?>
+
+<?php
+require("../validate.php");
+// $tb = '';
+$email ='';
+$password ='';
+$tb ='';
+
+if (isset($_POST['login_user'])) {
+    
+
+    $is_validated = true;
+    $errorEmail = $errorPassword = "";
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $hash_password = md5($password);
+    if ($email == "" || $password == "") {
+      $is_validated = false;
+      $tb = "Vui lòng nhập các ô còn thiếu";
+    }
+    if (checkEmailExist($email) == ""){
+      $is_validated = false;
+      $errorEmail = "Email không tồn tại";
+    }
+
+    $sql = "SELECT password FROM user WHERE email = '$email' AND active = 1";
+    $ketqua = $conn->query($sql);
+    if ($ketqua->num_rows == 0){
+      $tb = 'Sai email hoặc mật khẩu'; 
+    } else {
+      $password = $ketqua->fetch_array()['password'];
+      if($password == $hash_password) {
+          $_SESSION["email_user"] = $email;
+          if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) 
+            header('location: check_out.php');
+          else
+            header('location: my_account.php');
+      }
+      else $tb = 'Sai email hoặc mật khẩu'; 
+    }
+    if ($email == "" || $password == "") {
+      $is_validated = false;
+      $tb = "Vui lòng nhập các ô còn thiếu";
+    }
+    if ($is_validated) {
+    $_SESSION["email_user"] = $email;
+    if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) 
+      header('location: check_out.php');
+    else
+      header('location: my_account.php');
+    }
+}
+?>
+<div class="row d-flex justify-content-center align-items-center h-100">
+  <div class="col-lg-12 col-xl-11">
+    <div class="card-body p-md-5">
+      <div class="row justify-content-center">
+        <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+        <?php
+          // if (isset($_COOKIE['success'])) {
+          //   echo '<div class="mb-2 text-center"><div class="alert alert-success">'.$_COOKIE['success'].'</div></div>';
+          // }
+          if (isset($_SESSION['success'])) {
+            echo '<div class="mb-2 text-center"><div class="alert alert-success">'.$_SESSION['success'].'</div></div>';
+          }
+        ?>
+          <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4"> 
+            Đăng nhập 
+          </p>
+          <form class="mx-1 mx-md-4" action="<?php echo $_SERVER['PHP_SELF']?>" accept-charset="UTF-8" method="post">
+            <div class="d-flex flex-row align-items-center mb-4">
+              <div class="input-group flex-nowrap">
+                <span class="input-group-text"><i class="fa-light fa-envelope"></i></span>
+                <input type="text" class="form-control" name="email" value="<?php echo $email; ?>" placeholder="Email">
+              </div>
+            </div>
+            <div class="d-flex flex-row align-items-center mb-4">
+              <div class="input-group flex-nowrap">
+                <span class="input-group-text"><i class="fa-light fa-key"></i></span>
+                <input type="password" class="form-control" name="password" value="<?php echo $password; ?>" placeholder="Password">
+              </div>
+            </div>
+            <p>
+              Bạn chưa có tài khoản?
+              <a href="/AssignmentWeb/sign_up.php">Đăng kí ngay.</a>
+            </p>
+            <p class="mt-2 mb-2">
+              Quên mật khẩu <a href="/AssignmentWeb/auth/forgot_password.php">click here</a>
+            </p>
+                <?php 
+                    if(!empty($tb)) {
+                      echo '<div class="alert alert-danger">'.$tb. '</div>';
+                    }
+                    else if(!empty($errorEmail)) {
+                        echo '<div class="alert alert-danger">'.$errorEmail. '</div>';
+                    } else if (!empty($errorPassword)) {
+                      echo '<div class="alert alert-danger">'.$errorPassword. '</div>';
+                    } 
+
+                ?>
+                <?php 
+                    
+                ?>
+            <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+              <input type="submit" name="login_user" value="Login" class="btn btn-primary" data-disable-with="Create account" />
+            </div>
+        </form>        
+    </div>
+        <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center justify-content-center order-1 order-lg-2">
+          <img class="img-fluid rounded w-75" alt="Login image" src="../public/img/login.png" />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+    require '../includes/footer.php';
+?>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ policy.php
@@ -0,0 +1,141 @@
+<?php
+session_start();
+ob_start();
+$rootPath = '/Lap_trinh_web';
+require_once './database/DB.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chính sách</title>
+    <link rel="stylesheet"  href="https://site-assets.fontawesome.com/releases/v6.1.2/css/all.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="./public/css/base.css">
+    <!-- <link rel="stylesheet" href="./public/css/home.css"> -->
+</head>
+<body>
+<?php
+    require './includes/header.php';
+    require './includes/navbar.php';
+?>
+
+<div class="container pt-5 pb-5">
+    <div class="row">
+        <!-- <div class="col-xl-9 col-md-8 col-sm-6"> -->
+        <div class="col-lg-9 col-md-10 m-auto">
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                           A. Điều kiện nhượng quyền
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <p>- Chứng minh nhân dân hợp lệ, giấy xác nhận không phạm tội, đủ tư cách để xin các giấy phép liên quan.</p>
+                            <p>- Có hứng thú với ngành thức uống, chấp nhận văn hóa thương hiệu và triết lí kinh doanh, toàn tâm toàn ý cho việc kinh doanh, tuân theo sự thống nhất điều hành của công ty và có tinh thần hợp tác cao.</p>
+                            <p>- Bên nhượng quyền thương hiệu có thể trực tiếp tham gia khóa đào tạo, vận hành và quản lý cửa tiệm toàn thời gian ( Trước khi khai trương, phải đến công ty để tham gia học đào tạo, mỗi ngày cần học từ 7-8 tiếng, 5-7 ngày, tùy theo thực tế năng lực học tập để quyết định).</p>
+                            <p>- Có khả năng chịu rủi ro nhất định, có đủ kinh phí hoạt động ( nguồn vốn hợp pháp), địa điểm, nhân sự …</p>
+                            <p>- Không cùng lúc điều hành các doanh nghiệp có liên quan hoặc có tính cạnh tranh.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            B. Quy định thanh toán
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <h6>Quý khách có thể thanh toán bằng tiền mặt hoặc chuyển khoản qua ngân hàng</h6>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Ngân hàng</th>
+                                        <th scope="col">STK</th>
+                                        <th scope="col">Chủ tài khoản</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td scope="row">OCB</td>
+                                        <td>0004100040716008</td>
+                                        <td>Nguyễn Đức Bình</td>
+                                    </tr>
+                                    <tr>
+                                        <td scope="row">BIDV</td>
+                                        <td>6201250658</td>
+                                        <td>Nguyễn Đức Bình</td>
+                                    </tr>
+                                    <tr>
+                                        <td scope="row">Momo</td>
+                                        <td>03944333666</td>
+                                        <td>Nguyễn Đức Bình</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            C. Hướng dẫn mua hàng
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <p class="h6">1.Thêm sản phẩm vào giỏ hàng</p>
+                            <p>
+                                Chọn mục Menu góc trái bên trên để hiển thị các danh mục Sau đó click vào mục sản phẩm hoặc click vào nút <button class="btn btn-warning"><i class="fa-light fa-cart-shopping"></i></button> để thêm sản phẩm vào giỏ.
+                            </p>
+                            <p class="h6">2.Thanh toán đơn hàng</p>
+                            <p>
+                                Khách hàng cần <a href="#">đăng nhập</a> để đặt hàng (<a href="<?php echo $rootPath?>/sign_up.php">đăng kí</a> nếu bạn chưa có tài khoản). Sau đó bạn nhập các thông tin cần thiết theo mẫu để nhân viên xác nhận đơn hàng. 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="col-xl-3 col-md-4 d-none d-md-block d-xl-block d-xxl-none"> 
+            <img src="<?php echo $rootPath?>/public/img/logo.jpg" class="img-fluid mb-2 rounded" alt="">
+            <img src="<?php echo $rootPath?>/public/img/logo.jpg" class="img-fluid mb-2 rounded" alt="">
+            <img src="<?php echo $rootPath?>/public/img/logo.jpg" class="img-fluid mb-2 rounded" alt="">
+        </div> -->
+    </div>
+</div>
+
+<?php
+$conn->close();
+    require './includes/footer.php';
+?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="./public/javascripts/loadCartHeader.js"></script>
+
+<script>
+
+  $(document).ready(function() {
+      loadCartAjax();
+
+      $(window).scroll(function(){
+            if($(this).scrollTop()>114){
+            $("#navbar-top").addClass('fix-nav')
+            }else{
+                $("#navbar-top").removeClass('fix-nav')
+            }}
+        )
+  });
+</script>
+<script src="./public/javascripts/liveSearch.js"></script>
+
+</body>
+</html>
