d3de401490a64a1e724ffed6df1c49cf1de1674e
Nguyễn Công Anh Luân <conganhluan@gmail.com>
2023-12-05 21:03:47 +0700

    cập nhật title

--- post.php
+++ post.php
@@ -3,14 +3,23 @@ session_start();
 ob_start();
 $rootPath = '/Lap_trinh_web';
 require_once './database/DB.php';
+require './includes/header.php';
+require './includes/navbar.php';
+
+$id = $_GET['postId'];
+$sqlShowPost = "SELECT * FROM post WHERE post_id=$id";
+    
+$post= $conn->query($sqlShowPost);
+$row = $post->fetch_assoc();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tin tức</title>
+    <title>Tin tức<?php if($_GET['postId']) {echo ": ", $row['title'];}?></title>
     <link rel="stylesheet"  href="https://site-assets.fontawesome.com/releases/v6.1.2/css/all.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
@@ -18,18 +27,6 @@ require_once './database/DB.php';
     <link rel="stylesheet" href="./public/css/contact.css">
 </head>
 <body>
-<?php
-    require './includes/header.php';
-    require './includes/navbar.php';
-    ?>
-
-<?php
-    $id = $_GET['postId'];
-    $sqlShowPost = "SELECT * FROM post WHERE post_id=$id";
-    
-    $post= $conn->query($sqlShowPost);
-    $row = $post->fetch_assoc();
-?>
 
 <div class="container-fluid bg-light p-xxl-5 p-md-3">
     <div class="col-lg-8 col-md-10 m-auto text-center py-5 px-3" style="box-shadow: 0 10px 20px rgb(0 0 0 / 10%);">
