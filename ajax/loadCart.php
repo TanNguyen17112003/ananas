1182a37c6f9c8da99fa386af307b94ff3241301b
Trần Đan Huy <92442692+GedusX@users.noreply.github.com>
2024-05-02 22:45:47 +0700

    update the logic in cart

--- ajax/loadCart.php
+++ ajax/loadCart.php
@@ -26,7 +26,44 @@ if (isset($_SESSION["cart"]) && !empty($_SESSION["cart"])) {
                             </tr>
                         </thead>
                         <tbody>';
+    $checkoutDisabled = "";
     foreach ($_SESSION['cart'] as $key => $value) {
+
+        if (!isset($value['id'])){
+            unset($_SESSION['cart'][$key]);
+            continue;
+        }
+        $productID = $value["id"];
+        $productSize = $value["size"];
+        $sqlFindProduct = "SELECT * FROM product_instock WHERE  product_instock.product_id = '$productID' AND product_instock.size = '$productSize' LIMIT 1";
+        $product = $conn->query($sqlFindProduct);
+        $row = $product->fetch_assoc();
+        $maxQuantity = $row['quantity'];
+        $quantity = $value['quantity'];
+        if ($value['quantity']>$maxQuantity){
+            $quantity = $maxQuantity;
+            $_SESSION['cart'][$productID.'_'.$productSize]['quantity'] = $maxQuantity;
+        }
+        $addDisabled = "";
+        $subDisabled = "";
+        $leftBadge = '<div class="pb-3">
+        <span class="badge bg-success" id="product-quantity">Còn '.$maxQuantity.' sản phẩm </span>
+        </div>';
+        if ($maxQuantity<1){
+            $addDisabled = "disabled";
+            $subDisabled = "disabled";
+            $checkoutDisabled = "disabled";
+            $leftBadge = '<div class="pb-3">
+        <span class="badge bg-danger" id="product-quantity">Hết hàng</span>
+        </div>';
+        }
+        if ($maxQuantity <= $quantity){
+            $addDisabled = "disabled";
+        }
+        if (0 == $quantity){
+            $subDisabled = "disabled";
+        }
+
         $cart .=    '<tr>           
                                    <td scope="row" class="align-middle">
     <div class="container">
@@ -38,6 +75,7 @@ if (isset($_SESSION["cart"]) && !empty($_SESSION["cart"])) {
                 <div class="d-flex flex-column justify-content-center">
                     <p>' . $value['name'] . '</p>
                     <p>Mã sản phẩm: <strong>#' . $value['id'] . '</strong></p>
+                    '.$leftBadge.'
                 </div>
             </div>
         </div>
@@ -46,19 +84,20 @@ if (isset($_SESSION["cart"]) && !empty($_SESSION["cart"])) {
                                     <td class="text-center align-middle">' . number_format($value['price']) . ' <sup>đ</sup></td>
                                     <td class="text-center align-middle">
                                         <div class="d-flex align-items-center justify-content-center">
-                                            <button class="btn btn-secondary btn-sm ms-1" onclick="subCartQty(\'' . $value['id'] . '_' . $value['size'] . '\')">-</button>
-                                            <button class="btn btn-light disabled">' . $value['quantity'] . '</button>
-                                            <button class="btn btn-secondary btn-sm me-1" onclick="addCartQty(\'' . $value['id'] . '_' . $value['size'] . '\')">+</button>
+                                            <button class="btn btn-secondary btn-sm ms-1" onclick="subCartQty(\'' . $value['id'] . '_' . $value['size'] . '\')" '.$subDisabled.'>-</button>
+                                            <button class="btn btn-light disabled" id="' . $value['id'] . '_' . $value['size'] . '" min="1" max="'.$maxQuantity.'">' . $quantity . '</button>
+                                            <button class="btn btn-secondary btn-sm me-1" onclick="addCartQty(\'' . $value['id'] . '_' . $value['size'] . '\')" '.$addDisabled.'>+</button>
                                         </div>
                                     </td>
                                     <td class="text-center align-middle">' . $value['size'] . '</td>
-                                    <td class="text-center text-danger align-middle"><strong>' . number_format($value['price'] * $value['quantity']) . ' <sup>đ</sup></strong></td>
+                                    <td class="text-center text-danger align-middle"><strong>' . number_format($value['price'] * $quantity) . ' <sup>đ</sup></strong></td>
                                     <td class="text-center align-middle">
                                         <button class="btn btn-danger" onclick="deleteCartItem(\'' . $value['id'] . '_' . $value['size'] . '\')"><i class="fa-solid fa-trash-can"></i></button>
                                     </td>
                                 </tr>';
-        $totalBill += $value['price'] * $value['quantity'];
+        $totalBill += $value['price'] * $quantity;
     }
+    
     $cart .=    '</tbody>
                             </table>
                         </div>
@@ -81,7 +120,7 @@ if (isset($_SESSION["cart"]) && !empty($_SESSION["cart"])) {
                                     </table>
                                 </div>
                                 <div class="row">
-                                    <a href="' . $rootPath . '/customer/check_out.php" class="btn btn-primary"><strong>Đặt hàng</strong></a>
+                                    <a href="' . $rootPath . '/customer/check_out.php" class="btn btn-primary '.$checkoutDisabled.'"><strong>Đặt hàng</strong></a>
                                     <a href="' . $rootPath . '/product.php" class="btn btn-outline-warning mt-2">Tiếp tục mua hàng</a>
                                 </div>
                             </div>
